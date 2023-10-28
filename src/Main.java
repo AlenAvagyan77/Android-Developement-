5a9def2ef5185301b53abbcd4f80a0dc1213165e
AlenAvagyan77 <alen_avagyan_03@mail.ru>
2023-10-28 13:43:55 +0400

    My first task for Android Development in Git

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            int square = i * i;
+            System.out.println("The square of " + i + " is " + square);
+        }
+    }
+}
