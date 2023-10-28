8bfa83af9302ff6c978edc5838621178d5766bb0
AlenAvagyan77 <alen_avagyan_03@mail.ru>
2023-10-28 15:02:49 +0400

    My first task for Android Development in Git

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
+        theSquareOfNumber(10);
+        printTheNumberWithDividedMode(100);
+
+    }
+
+    public static void theSquareOfNumber(int number) {
+        /*
+        Print the values of the squares of the numbers 0-100 on the screen.
+         */
+        for (int i = 0; i <= number; i++) {
             int square = i * i;
             System.out.println("The square of " + i + " is " + square);
+
+        }
+    }
+
+    public static void printTheNumberWithDividedMode(int number) {
+        /*
+        Declare the variable by assigning a value to it, print the digits of the number on the screen step by step.
+        For example, for the given number 5479, type "5", "4", "7", "9" to avoid recall.
+         */
+        String numberStr = Integer.toString(number);
+        for (int i = 0; i < numberStr.length(); i++) {
+            char digit = numberStr.charAt(i);
+            System.out.print("'" + digit + "',");
         }
     }
 }
